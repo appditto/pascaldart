c425bd995d5c7274c10076377c2d5b3430e471c6
bbedward <bbedward@gmail.com>
2019-05-23 21:12:02 -0400

    Create signing namespace

---
+++ lib/signing.dart
@@ -0,0 +1,1 @@
+export 'src/signing/Signer.dart';
---
+++ lib/src/signing/Signer.dart
@@ -0,0 +1,18 @@
+import 'dart:typed_data';
+
+import 'package:pascaldart/src/common/Sha.dart';
+import 'package:pascaldart/src/common/model/keys/PrivateKey.dart';
+import 'package:pascaldart/src/crypto/keys.dart';
+import 'package:pascaldart/src/crypto/model/Signature.dart';
+
+class Signer {
+    /// Sign the sha256 of the given digest and return r and s
+    Signature signWithHash(PrivateKey privateKey, Uint8List digest) {
+      return Keys.sign(privateKey, Sha.sha256([digest]));
+    }
+
+    /// Sign digest
+    Signature signdigest(PrivateKey privateKey, Uint8List digest) {
+      return Keys.sign(privateKey, digest);
+    }
+}
